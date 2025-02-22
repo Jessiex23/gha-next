d27ad2fe3c59b2273220694ea2f605a39a5ef072
Jessiex23 <yanxu.jess@gmail.com>
2025-02-22 23:00:47 +1100

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -6,7 +6,7 @@ export default function Page() {
    return (
     <>
         <h1>App Router</h1>
-        <h2 style={{ textAlign: "center", color: "red" }}>Changed something</h2>
+        <h2 style={{ textAlign: "center", color: "purple" }}>Changed something</h2>
     </>
   );
 }
